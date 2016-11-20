68fad67442b1002c6120fb0b9147fe14e46ff58b
Denis Colliot <denis.colliot@gmail.com>
2016-11-20 20:33:50 +0100

    Contacts view & presenter

--- src/main/java/fr/fscf/contacts/client/ui/presenter/ContactsPresenter.java
+++ src/main/java/fr/fscf/contacts/client/ui/presenter/ContactsPresenter.java
@@ -1,5 +1,7 @@
 package fr.fscf.contacts.client.ui.presenter;
 
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.view.client.*;
 import com.google.inject.ImplementedBy;
 import fr.fscf.contacts.client.inject.Injector;
 import fr.fscf.contacts.client.navigation.Page;
@@ -7,8 +9,13 @@ import fr.fscf.contacts.client.navigation.PageRequest;
 import fr.fscf.contacts.client.ui.presenter.base.AbstractPagePresenter;
 import fr.fscf.contacts.client.ui.view.ContactsView;
 import fr.fscf.contacts.client.ui.view.base.ViewInterface;
+import fr.fscf.contacts.shared.dto.ContactDTO;
+import org.gwtbootstrap3.client.ui.Pagination;
+import org.gwtbootstrap3.client.ui.gwt.CellTable;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Contacts presenter.
@@ -17,13 +24,33 @@ import javax.inject.Inject;
  */
 public class ContactsPresenter extends AbstractPagePresenter<ContactsPresenter.View> {
 
+    private static final List<ContactDTO> MOCK_DATA = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 100; i++) {
+            final ContactDTO contactDTO = new ContactDTO();
+            contactDTO.setId((long) i);
+            contactDTO.setName("Name #" + i);
+            contactDTO.setFirstName("Firstname #" + i);
+            contactDTO.setEmail("contact_" + i + "@email.com");
+            MOCK_DATA.add(contactDTO);
+        }
+    }
+
     /**
      * View interface.
      */
     @ImplementedBy(ContactsView.class)
     public interface View extends ViewInterface {
 
+        CellTable<ContactDTO> getContactsTable();
+
+        Pagination getPagination();
+
     }
+
+    private SimplePager cellTablePager;
+    private AsyncDataProvider<ContactDTO> dataProvider;
 
     @Inject
     protected ContactsPresenter(final View view, final Injector injector) {
@@ -36,7 +63,36 @@ public class ContactsPresenter extends AbstractPagePresenter<ContactsPresenter.V
     }
 
     @Override
+    public void onBind() {
+        cellTablePager = new SimplePager();
+        cellTablePager.setDisplay(view.getContactsTable());
+
+        dataProvider = new AsyncDataProvider<ContactDTO>() {
+            @Override
+            protected void onRangeChanged(final HasData<ContactDTO> display) {
+                final Range range = display.getVisibleRange();
+                final int start = range.getStart();
+                final int length = range.getLength();
+
+                dataProvider.updateRowCount(MOCK_DATA.size(), true); // Fires a 'onRangeChanged' event.
+
+                // Create the data to push into the view. At this point, you could send
+                // an asynchronous RPC request to a server.
+
+                // Push the data into the list.
+                view.getContactsTable().setRowData(start, MOCK_DATA.subList(start, start + length));
+
+                rebuildTable();
+            }
+        };
+        dataProvider.addDataDisplay(view.getContactsTable());
+    }
+
+    @Override
     public void onPageRequest(final PageRequest request) {
-        // TODO
+    }
+
+    private void rebuildTable() {
+        view.getPagination().rebuild(cellTablePager);
     }
 }
---
+++ src/main/java/fr/fscf/contacts/shared/dto/ContactDTO.java
@@ -0,0 +1,120 @@
+package fr.fscf.contacts.shared.dto;
+
+import fr.fscf.contacts.shared.dto.base.AbstractEntityDTO;
+
+/**
+ * Application contact DTO.
+ */
+public class ContactDTO extends AbstractEntityDTO<Long> {
+
+    private String name;
+
+    private String firstName;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private String additionalAddress;
+
+    private String zipCode;
+
+    private String city;
+
+    private String cedex;
+
+    private String email2;
+
+    private String phone2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAdditionalAddress() {
+        return additionalAddress;
+    }
+
+    public void setAdditionalAddress(String additionalAddress) {
+        this.additionalAddress = additionalAddress;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCedex() {
+        return cedex;
+    }
+
+    public void setCedex(String cedex) {
+        this.cedex = cedex;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+}
